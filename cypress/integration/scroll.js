8da5eda56c9f036d0309840183b99e519afd2edf
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-11-15 18:54:35 +0000

    add additional jquery unwrapping example

--- cypress/integration/scroll.js
+++ cypress/integration/scroll.js
@@ -1,6 +1,7 @@
 beforeEach(() => {
   cy.visit('/');
 });
+
 it('scrolls to top', () => {
   cy.scrollTo('bottom').window().its('scrollY').should('not.equal', 0);
 
@@ -8,12 +9,25 @@ it('scrolls to top', () => {
 
   cy.window().its('scrollY').should('equal', 0);
 });
-it('scrolls to target', () => {
+
+it('scrolls to target - cy.$$ + jQuery.offset()', () => {
   cy.scrollTo('bottom').window().its('scrollY').should('not.equal', 0);
 
   cy.get('[data-testid=scroll-to-element]').click();
 
+  // Using cy.$$ (jQuery) + offset().top
   cy.window()
     .its('scrollY')
     .should('equal', cy.$$('[data-testid=scroll-target]').offset().top);
+});
+
+it('scrolls to target - cy.get + unwrap jQuery reference', () => {
+  cy.scrollTo('bottom').window().its('scrollY').should('not.equal', 0);
+
+  cy.get('[data-testid=scroll-to-element]').click();
+
+  // Using cy.get() + unwrapping the jQuery reference with `el[0]` or `el.get(0)`
+  cy.get('[data-testid=scroll-target]')
+    .then((element) => element[0].offsetTop)
+    .then((offset) => cy.window().its('scrollY').should('equal', offset));
 });
