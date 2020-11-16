9876b82dfd9a06804a52ea9d6bc49de9fd60a43c
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-11-16 17:37:54 +0000

    move scrolling to bottom to beforeEach

--- cypress/integration/scroll.js
+++ cypress/integration/scroll.js
@@ -1,18 +1,15 @@
 beforeEach(() => {
   cy.visit('/');
+  cy.scrollTo('bottom').window().its('scrollY').should('not.equal', 0);
 });
 
 it('scrolls to top', () => {
-  cy.scrollTo('bottom').window().its('scrollY').should('not.equal', 0);
-
   cy.get('[data-testid=scroll-to-top]').click();
 
   cy.window().its('scrollY').should('equal', 0);
 });
 
 it('scrolls to target - cy.$$ + jQuery.offset()', () => {
-  cy.scrollTo('bottom').window().its('scrollY').should('not.equal', 0);
-
   cy.get('[data-testid=scroll-to-element]').click();
 
   // Using cy.$$ (jQuery) + offset().top
@@ -22,8 +19,6 @@ it('scrolls to target - cy.$$ + jQuery.offset()', () => {
 });
 
 it('scrolls to target - cy.get + unwrap jQuery reference', () => {
-  cy.scrollTo('bottom').window().its('scrollY').should('not.equal', 0);
-
   cy.get('[data-testid=scroll-to-element]').click();
 
   // Using cy.get() + unwrapping the jQuery reference with `el[0]` or `el.get(0)`
